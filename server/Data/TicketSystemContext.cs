3371866ce2055052062f47db6c74f94f53c80f8e
ranjitjc <ranjitjc@gmail.com>
2017-01-13 02:31:44 -0500

    Backend Authenticatoin

--- server/Data/TicketSystemContext.cs
+++ server/Data/TicketSystemContext.cs
@@ -10,10 +10,13 @@ namespace TicketSystem.Data
 
         public DbSet<Models.TicketStatus> TicketStatuses { get; set; }
 
+        public DbSet<Models.User> Users { get; set; }
+
 
          protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Models.TicketStatus>().ToTable("statuses");
+            modelBuilder.Entity<Models.User>().ToTable("users");
         }
     }
 }
