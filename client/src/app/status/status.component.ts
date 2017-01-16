40d205e8ad0ce9b63ce3771a27a2fd6f1ce8f8ed
Jeyachandran <ranjit.jeyachandran@orthoclinicaldiagnostics.com>
2017-01-16 06:40:08 -0500

    handled webapi errors

--- client/src/app/status/status.component.ts
+++ client/src/app/status/status.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { StatusService, TicketStatus } from '../services/status.service';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
-  styleUrls: ['./status.component.css'],
+  styleUrls: ['./status.component.scss'],
     providers: [
-        StatusService
+        StatusService,
     ]
 })
 export class StatusComponent extends OnInit {
+    isLoading:Boolean;
+    isError:Boolean;
+    ErrorMessage:string;
+    statuses: TicketStatus[] = [];
 
     constructor(private _service: StatusService) {
         super();
+        this.isLoading= true;
+        this.isError = false;
+        
     }
 
     ngOnInit() {
-        this._service.loadData().then(data => {
+        this._service.GetAll().subscribe(
+            data => {
             this.statuses = data;
-        })
+            this.isLoading= false;
+            },
+            err => {
+                console.error("StatusComponent.ngOnit :" + err);
+                this.ErrorMessage = err;
+                this.isLoading = false;
+                this.isError = true;
+            }
+        );
     }
 
-    statuses: TicketStatus[] = [];
+    
 }
