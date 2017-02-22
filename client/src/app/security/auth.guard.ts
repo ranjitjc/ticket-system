d108c72febf8e1ab1cbfb8bcab34b5d660d6d221
ranjitjc <ranjiitjc>
2017-02-22 05:18:55 -0500

    AuthGuard Implementation

--- client/src/app/security/auth.guard.ts
+++ client/src/app/security/auth.guard.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 
+import { AuthType, environment } from '../../environments/environment';
+
+import { AuthenticationService  } from '../services/services.module';
+
+
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router,
+    private _authenticationService: AuthenticationService) { }
 
     canActivate() {
         if (localStorage.getItem('currentUser')) {
@@ -13,7 +19,13 @@ export class AuthGuard implements CanActivate {
         }
 
         // not logged in so redirect to login page
-        this.router.navigate(['login']);
-        return false;
+        if (environment.authType == AuthType.FORMS){
+            this.router.navigate(['login']);
+            return false;
+        }else{
+            this._authenticationService.login().subscribe();
+            return true;
+        }
+        
     }
 }
